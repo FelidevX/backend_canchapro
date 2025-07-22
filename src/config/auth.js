0be972ce7c7a694e91d927186401015b3d15aa2c
FelidevX <felipe2002ft@gmail.com>
2025-07-21 21:24:32 -0400

    callback google

--- src/config/auth.js
+++ src/config/auth.js
@@ -4,7 +4,7 @@ const { OAuth2Client } = require('google-auth-library');
 // Configuración de las credenciales de OAuth2 para Google
 const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;        // ID del cliente de Google OAuth
 const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET; // Secreto del cliente de Google OAuth
-const REDIRECT_URI = 'http://localhost:3000/auth/google/callback'; // URL de redirección después de la autenticación
+const REDIRECT_URI = 'https://backend-canchapro.onrender.com/auth/google/callback'; // URL de redirección después de la autenticación
 const JWT_SECRET = process.env.JWT_SECRET || 'secretjwt';     // Secreto para firmar los tokens JWT
 
 const oAuth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
